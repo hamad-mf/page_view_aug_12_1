28b4fe43dbd4e91988ff355d61fb4f7ad481607d
hamad-mf <hamadmf100@gmail.com>
2024-08-12 09:58:03 +0530

    first

---
+++ android/app/src/main/kotlin/com/example/page_view_aug_12_1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.page_view_aug_12_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          body: SafeArea(
+              child: PageView(
+        children: [
+          Container(
+            color: Colors.red,
+          ),
+          Container(
+            color: Colors.yellow,
+          )
+        ],
+      ))),
+    );
+  }
+}
